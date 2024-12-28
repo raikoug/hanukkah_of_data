31e1b0e07625ef6fdf8b87dbac6abfa7acaae030
raikoug <raikoug@gmail.com>
2024-12-29 00:42:52 +0100

    Add Order and OrderItem classes with datetime conversion

---
+++ python_hod/orders.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional, List
+from .product import Product
+
+
+@dataclass
+class OrderItem:
+    orderid: int
+    sku: str
+    qty: int
+    unit_price: float
+    sku_item: Optional['Product'] = None
+
+
+@dataclass
+class Order:
+    orderid: int
+    customerid: int
+    ordered: datetime
+    shipped: datetime
+    items: Optional[List[OrderItem]]
+    total: float
+
+    def __post_init__(self):
+        # Convert ordered and shipped from string to datetime
+        self.ordered = datetime.strptime(self.ordered, '%Y-%m-%d %H:%M:%S')
+        self.shipped = datetime.strptime(self.shipped, '%Y-%m-%d %H:%M:%S')
+
