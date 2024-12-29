03284426e2523dff46d4676ba8a090ccb497b72c
raikoug <raikoug@gmail.com>
2024-12-29 02:30:13 +0100

    Refactor solve function to filter customers by city and improve SKU case sensitivity for 'Senior Cat' items

--- solutions/5784/python/05.py
+++ solutions/5784/python/05.py
@@ -7,20 +7,20 @@ year = int(Path(__file__).parent.parent.stem)
 sys.path.append(f"{Path(__file__).parent.parent.parent.parent.absolute()}")
 from python_hod import HOD, Customer, Order
 
-def solve(hod: HOD) -> Customer:    
+def solve(hod: HOD) -> Customer:
+    target = "Staten Island"
     def senior_cat_items(order: Order) -> int:
-        return len([item for item in order.items if "Senior Cat" in item.sku_item.desc ])
+        return len([item for item in order.items if "senior cat" in item.sku_item.desc.lower() ])
     
     best_senior_cat_items = 0
     best_serionr_cat_customer = None
-    for customer in hod.customers:
+    for customer in [c for c in hod.customers if target in c.citystatezip]:
         tmp_senior_cat_items = 0
         orders = hod.get_orders_from_customer_id(customer.customerid)
         for order in orders:
             tmp_senior_cat_items += senior_cat_items(order)
         
         if tmp_senior_cat_items > best_senior_cat_items:
-            print(f"New best: {tmp_senior_cat_items} for {customer.customerid} from {customer.}")
             best_senior_cat_items = tmp_senior_cat_items
             best_serionr_cat_customer = customer
     
